55e3771ade47bdb9c92f8445eed2bbfe2a7b0ea8
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2026-01-10 15:29:27 +0700

    first commit

--- database/seed_credit.sql
+++
@@ -1,3 +0,0 @@
--- Insert Credit Setting for Admin (Central Pool)
-INSERT INTO `credit_setting` (`credit_balance`, `credit_min`, `user_id`) VALUES
-(50000, 10000, 1);
--- database/seed_customers.sql
+++
@@ -1,5 +0,0 @@
--- Insert Customers
-INSERT INTO `customer` (`customer_name`, `customer_phone`, `customer_email`) VALUES
-('Thai Bev', '0811111111', 'contact@thaibev.com'),
-('CP All', '0822222222', 'support@cpall.co.th'),
-('True Corp', '0833333333', 'info@truecorp.co.th');
